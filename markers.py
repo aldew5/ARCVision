4415475269d9cfcf8cf2652055e71c8a8d625400
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-12-28 13:19:44 -0500

    Update markers.py

--- markers.py
+++ markers.py
@@ -10,27 +10,27 @@ from augment import augment
 # variable name
 
 class Marker(object):
-    def __init__(self, label):
+    def __init__(self, label, image, frame, corners, frame_width, frame_height):
         self.label = label
+        self.frame = frame
+        self.image = image
+        self.corners = corners
+        self.width, self.height = 200, 200
+        self.frame_width, self.frame_height = frame_width, frame_height
       
         
 
     def display(self, video_frame, image):
-        augment(self.label, frame, corners, (width, height), video_frame,\
-                (frame_width, frame_height), image)
+        augment(self.label, self.frame, self.corners, (self.width, self.height), video_frame,\
+                (self.frame_width, self.frame_height), image)
 
 
 class Variable(Marker):
     def __init__(self, image, label, frame, corners, frame_width, frame_height):
-        self.label = label
-        self.frame = frame
-        self.image = image
-        self.name = input("Please enter a name for the variable: ")
-        self.value = input("Please enter a value for the variable: ")
-        self.corners = corners
-        self.width = 200
-        self.height = 200
-        self.frame_width, self.frame_height = frame_width, frame_height
+        Marker.__init__(self, label, image, frame, corners, frame_width, frame_height)
+        
+        self.name = "TEST" #input("Please enter a name for the variable: ")
+        self.value = 7 #input("Please enter a value for the variable: ")
         
 
     def display(self):
@@ -38,8 +38,8 @@ class Variable(Marker):
         blank[:, 0:200] = (255, 255, 255)
 
         font = cv2.FONT_HERSHEY_SIMPLEX
-        bc = (10, 500)
-        text = self.name + " = " + self.value
+        bc = (10, 100)
+        text = self.name + " = " + str(self.value)
 
         
         cv2.putText(blank, text, bc, cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0),\
@@ -47,8 +47,14 @@ class Variable(Marker):
         
         blank_frame = cv2.resize(blank, (200, 200))
         
-        augment(self.label, self.frame, self.corners, (self.width, self.height), blank_frame,\
-                (self.frame_width, self.frame_height), self.frame)
+        Marker.display(self, blank_frame, self.image)
+        
+    def update(self, image, frame, corners):
+        self.image = image
+        self.frame = frame
+        self.corners = corners
+        
+        
 
     def print(self):
         print(self.name, ":", self.value)
