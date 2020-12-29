43efc94db014bed8a762a3d524cda99559afc49a
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-12-29 18:54:17 -0500

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,14 @@
+import pygame as pg
+
+pg.init()
+
+win = pg.display.set_mode(1000, 1000)
+pg.display.set_caption("Science Fair 2020-2021")
+
+clock = pg.time.Clock()
+
+#have a menu with different phases?
+# One being the intro screen ?
+
+def drawWindow():
+    
