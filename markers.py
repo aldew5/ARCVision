503e4e1a46e64f05a1834dc68b2f77a54db091e3
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-12-29 18:56:39 -0500

    Created Operator class

--- markers.py
+++ markers.py
@@ -17,7 +17,12 @@ class Marker(object):
         self.corners = corners
         self.width, self.height = 200, 200
         self.frame_width, self.frame_height = frame_width, frame_height
-      
+        
+    
+    def update(self, image, frame, corners):
+        self.image = image
+        self.frame = frame
+        self.corners = corners  
         
 
     def display(self, video_frame, image):
@@ -31,6 +36,12 @@ class Variable(Marker):
         
         self.name = "TEST" #input("Please enter a name for the variable: ")
         self.value = 7 #input("Please enter a value for the variable: ")
+        
+        if (type(self.value) == int or type(self.value) == float):
+            self.type = "num"
+        else if (type(self.value) == str):
+            self.type = "string"
+        
         
 
     def display(self):
@@ -49,16 +60,57 @@ class Variable(Marker):
         
         Marker.display(self, blank_frame, self.image)
         
-    def update(self, image, frame, corners):
-        self.image = image
-        self.frame = frame
-        self.corners = corners
-        
         
 
     def print(self):
         print(self.name, ":", self.value)
     
-
     
+    
+
+class Operator(Marker):
+    def __init__(self, image, label, frame, corners, frame_width, frame_height):
+        Marker.__init__(self, label, image, frame, corners, frame_width, frame_height)
+        self.oper = '+' #input("What operation would you like to perform: ")
+    
+    def display(self):
+        blank = np.zeros((200, 200, 3), np.uint8)
+        blank[:, 0:200] = (255, 255, 255)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        bc = (10, 100)
+        
+        cv2.putText(blank, self.oper, bc, cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0),\
+                    2)
+        
+        blank_frame = cv2.resize(blank, (200, 200))
+        
+        Marker.display(self, blank_frame, self.image)
+        
+    def compute(self, var1, var2=None, value=None):
+        if (var2 == None):
+            if (var1.type == "num" and (type(value) == int or type(value) == float)):
+                var1.value += value
+            else if (var1.type == "string" and type(value) == str):
+                var1.value += value
+            else:
+                print("ERROR")
+                print("Incompatible data types")
+        
+        else if (value == None):
+            if (var1.type == var2.type):
+                var1.value += var2.value
+            else:
+                print("ERROR")
+                print("Incompatible data types")
+        
+class Loop(Marker):
+    def __init__(self, image, label, frame, corners, frame_width, frame_height, iter_count):
+        Marker.__init__(self, label, image, frame, corners, frame_width, frame_height)
+        self.iter_count = iter_count
+    
+    
+            
+        
+        
     
