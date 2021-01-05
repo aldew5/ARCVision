f8dcbf43255d1f2ffd01e94936eddc61460bdef9
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2021-01-04 20:31:59 -0500

    Button and CheckBox classes

---
+++ GUI/gui_objects.py
@@ -0,0 +1,36 @@
+import pygame as pg
+
+class Button(object):
+    def __init__(self, color, x, y, width, height):
+        self.color = color
+        self.x, self.y = (x, y)
+        self.width, self.height = (width, height)
+        
+    def draw(self, win, outline=None):
+        if outline:
+            pg.draw.rect(win, outline, (self.x -2, self.y-2, self.width+4, self.height+4), 0)
+        
+        pg.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
+        
+##        if (self.text != ""):
+##            font = pg.font.SysFont("comicsans", 60)
+##            text = font.render(self.text, 1, (0,0,0))
+##            win.blit(text, (self.x + (self.width /2 - text.get_width() /2), self.y + (self.height /2 - text.get_height() /2)))
+            
+    def isHover(self, pos) -> bool:
+        if (pos[0] > self.x and pos[0] < self.x + self.width):
+            if (pos[1] > self.y and pos[1] < self.y + self.height):
+                return True
+                
+        return False
+
+class CheckBox(Button):
+    def __init__(self, x, y, width, height):
+        Button.__init__(self, (200,255,255), x, y, width, height)
+        self.isToggled = False
+
+    def draw(self, win):
+        if not self.isToggled:
+            pg.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
+        else:
+            pg.draw.rect(win, (0,0,0), (self.x, self.y, self.width, self.height), 0)
