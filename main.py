3473bff1185d697c19b14d780555fe79a62884bc
Alec <alec@cisudbury.ca>
2021-01-07 23:40:05 -0500

    cleaned up code and added comments

--- main.py
+++ main.py
@@ -7,70 +7,88 @@ from distance import distance
 from color_detection import detect_color
 
 
-flatten = lambda l: [item for sublist in l for item in sublist]
-
-
+# access webcame video stream
 cap = cv2.VideoCapture(0)
+# delare the aruco dictionary
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 parameters = aruco.DetectorParameters_create()
 
-rect = np.zeros((4, 2), dtype="float32")
+# store a dictionary of detected markers outside the main loop
+# so we can store their values 
 detected = {}
 
+# inintialize the dictionary
 for i in range(50):
     detected[i] = False
 
-# id to marker
+# keep lists of different markers
 variables = {}
 operators = {}
 
+# keep track of whether or not an operation has been performed
+# and set a timer between them
 timeout = 0
 updated = False
 
 
 while (cap.isOpened()):
-    #print("IN")
+    # capture frame by frame
     ret, frame = cap.read()
+    # take the dimensions of the image
     frame_height, frame_width, frame_channels = frame.shape
-    
+
+    # covert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # detect the markers and frame them
     corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict,\
                                                       parameters=parameters)
     frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
     img1 = frame
-    
+
+    # keep a list of the variables and operators currently
+    # in the frame
     curvar, curops = [], []
     
-
+    # we have detected at least one marker
     if (len(corners)):
+        # create a 1D array of the markers 
         ids2 = ids.flatten()
         
         eindex = -1
         for id in ids2:
+            # save the index in the corners array
+            # that corresponds to the current id
             eindex += 1
             
             if (id == 2):
+                # declare a new operator
                 if (not detected[id]):
                     oper = Operator(id, img1, eindex, frame, corners, frame_width, frame_height)
                     operators[id] = oper
+                # update the curops array
                 curops.append(operators[id])
             else:
+                # delcare a new varaible 
                 if (not detected[id]):
                     var = Variable(id, img1, eindex, frame, corners, frame_width, frame_height)
                     variables[id] = var
                 curvar.append(variables[id])
             detected[id] = True
         
-      
+
+        # loop through the variables currently in the frame
         for var in curvar:
            
             eindex = -1
             for id in ids2:
                 eindex += 1
+                # update them with the current parameters
                 if (id == var.id):
                     var.update(eindex, img1, frame, corners)
+            # augment
             var.display()
-            
+
+        # udpate the operators in the frame and display them
         for oper in curops:
             eindex = -1
             for id in ids2:
@@ -82,13 +100,15 @@ while (cap.isOpened()):
             
         # check for operation
         if (len(curops) and len(curvar) and not updated):
+            # confirm the operation with a red object in the frame
             if (detect_color(frame, "red")):
                 # keep track of variables used in operations
+                # (for multi-variable operations)
                 completed = {}
                 for i in range(50):
                     completed[i] = False
                     
-                # 2D list of valid operator varaible pairs
+                # 2D list of valid operator variable pairs
                 poss = []
                 # support single variable operations
                 for op in curops:  
@@ -104,16 +124,21 @@ while (cap.isOpened()):
                         br2 = corners[var.eindex][0][2]
                         bl2 = corners[var.eindex][0][3]
                         
+                        # compute the distance between the varaible and the
+                        # marker 
                         d = distance(tl, tr, br, bl, tl2, tr2, br2, bl2)
-                        
+
+                        # if they are close enough add them as a possible
+                        # operation to the poss array
                         if (d <= 300):
                             poss.append([var, op])
                             updated = True
                 
-                
+                # multi-variable operation
                 for i in poss:
                     for j in poss:
-                        # multi-variable operation
+                        # make sure the varaibles are different and the operators
+                        # are the same
                         if (i[0].id != j[0].id and i[1].id == j[1].id):
                             completed[i[0].id] = True
                             completed[j[0].id] = True
@@ -122,8 +147,10 @@ while (cap.isOpened()):
                 # single variable operations
                 for i in poss:
                     if (not completed[i[0].id]):
+                        # request the value that will be used in the computation
                         value = input("Please input a value to update " + var.name + " : ")
-                            
+
+                        # numerical computation
                         if (var.type == "num"):
                             value = int(value)
                                 
@@ -134,20 +161,23 @@ while (cap.isOpened()):
                         
                         
         
-        
+    # show the different frames
     cv2.imshow("augmented", img1)
     cv2.imshow("Frame Markers", frame_markers)
-    
+
+    # we completed an operation so update the timeout counter
     if (updated):
         timeout += 1
-    
+
+    # if we reach timeout == 50, we can perform another operation
     if (timeout == 50):
         updated = False
     
-    
+    # if we quite
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
+# close the frames
 cap.release()
 cv2.destroyAllWindows()
 
--- markers.py
+++ markers.py
@@ -4,11 +4,11 @@ from cv2 import aruco
 from augment import *
 
 
-
-# variable marker comes onto screen and student can type
-# variable name
-
 class Marker(object):
+    """
+    An abstract class that defines some methods all markers in the program
+    will use
+    """
     def __init__(self, id, eindex, image, frame, corners, frame_width, frame_height):
         self.id = id
         self.eindex = eindex
@@ -18,39 +18,64 @@ class Marker(object):
         self.width, self.height = 200, 200
         self.frame_width, self.frame_height = frame_width, frame_height
         
-    
+    # must be updated for each new frame
     def update(self, eindex, image, frame, corners):
         self.eindex = eindex
         self.image = image
         self.frame = frame
         self.corners = corners  
         
-
+    # augments the ArUco Marker
     def display(self, video_frame, image):
         augment(self.eindex, self.frame, self.corners, (self.width, self.height), video_frame,\
                 (self.frame_width, self.frame_height), image)
 
 
 class Variable(Marker):
+    """ Variable class """
+    
     def __init__(self, id, image, eindex, frame, corners, frame_width, frame_height):
         Marker.__init__(self, id, eindex, image, frame, corners, frame_width, frame_height)
 
+        # take user input for variable declaration
         self.type = input("Please declare the variable's type (String or Int): ")
         self.name = input("Please enter a name for the variable: ")
         self.value = input("Please enter a value for the variable: ")
         
-        if self.type == "String":
+        if self.type == "String" or self.type == "string":
             self.type = "string"
         elif self.type == "Int" or self.type == "int":
             self.type = "int"
 
+        # not a valid type
+        else:
+            print("ERROR")
+            print(self.type, "is not a valid variable type")
+
+            loop = True
+            # ask the user to input a valid type until they do so
+            while loop:
+                self.type = input("Please input a new type: ")
+
+                if self.type != "string" and self.type != "String" and self.type != "Int" and self.type != "int":
+                    print("ERROR")
+                    print(self.type, "is an invalid varaible type")
+                else:
+                    loop = False
+            
+
+        if self.type == "int":
             try:
-                self.value = int(self.value())
+                self.value = int(self.value)
+            # self.value couldn't be converted to an integer
+            # the input value was invalid
             except TypeError:
                 print("ERROR")
                 print("The value assigned to", self.name, "doesn't match the type")
 
                 loop = True
+                # request that the user input a valid number
+                # until they do so
                 while True:
                     try:
                         self.value = int(input("Please input a new value: "))
@@ -64,17 +89,21 @@ class Variable(Marker):
         
 
     def display(self):
+        # blank frame
         blank = np.zeros((200, 200, 3), np.uint8)
+        # set it to white
         blank[:, 0:200] = (255, 255, 255)
 
+        # write the variable text on the marker
         font = cv2.FONT_HERSHEY_SIMPLEX
         bc = (10, 100)
         text = self.name + " = " + str(self.value)
 
-        
+        # put the text in the frame
         cv2.putText(blank, text, bc, cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0),\
                     2)
-        
+
+        # create the frame and display it
         blank_frame = cv2.resize(blank, (200, 200))
         
         Marker.display(self, blank_frame, self.image)
@@ -82,15 +111,22 @@ class Variable(Marker):
         
 
     def print(self):
+        """ Neat printing format """
         print(self.name, ":", self.value)
         
     def set_value(self, value):
+        """ Setter for value """
         self.value = value 
     
     
     
 
 class Operator(Marker):
+    """
+    Operators are responsible for initiating operations between
+    variables, or a variable and a value
+    """
+    
     def __init__(self, id, image, eindex, frame, corners, frame_width, frame_height):
         Marker.__init__(self, id, eindex, image, frame, corners, frame_width, frame_height)
         self.oper = input("What operation would you like to perform: ")
@@ -110,7 +146,11 @@ class Operator(Marker):
         Marker.display(self, blank_frame, self.image)
         
     def compute(self, var1, var2=None, value=None):
+        """ Carry out an operation """
+
+        # it is a value, variable operation
         if (var2 == None):
+            # make sure the value given is compatible with numerical operations
             if (var1.type == "num" and (type(value) == int or type(value) == float)):
                 if (self.oper == '+'):
                     var1.set_value(var1.value + value)
@@ -120,7 +160,8 @@ class Operator(Marker):
                     var1.set_value(var1.value * value)
                 elif (self.oper == '/'):
                     var1.set_value(var1.value / value)
-          
+
+            # make sure the value is concatenable with a string
             elif (var1.type == "string" and type(value) == str):
                 if (self.oper == '+'):
                     var1.set_value(var1.value + value)
@@ -130,11 +171,15 @@ class Operator(Marker):
             else:
                 print("ERROR")
                 print("Incompatible data types")
-        
+
+        # variable, variable operation
         elif(value == None):
+            # the variables must have the same type
             if (var1.type == var2.type):
+                # complete the operation
                 if (self.oper == '+'):
                     var1.set_value(var1.value + var2.value)
+                # not a concatenation operation and the type isn't a string
                 elif (var1.type == "string"):
                     print("ERROR")
                     print("The", self.oper, "operation is undefined for objects of type string")
