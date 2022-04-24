d30fc42fe30970a161e74afbb36384ac9aa5a64f
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2022-04-24 12:40:44 -0400

    update comments

--- source/utils/augment.py
+++ source/utils/augment.py
@@ -21,12 +21,12 @@ def order_points(pts):
 def four_point_transform(image, points, width=0, height=0):
     """Computes a warped perspective that and a homography matrix
         mapping the image back to its original position"""
-    #upack points
+    # upack points
     rect = order_points(points)
     (tl, tr, br, bl) = rect
     
     if (width == 0 and height == 0):
-        #compute the width of the new image
+        # compute the width of the new image
         widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
         widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
         mxW = max(int(widthA), int(widthB))
@@ -36,9 +36,8 @@ def four_point_transform(image, points, width=0, height=0):
         heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
         mxH = max(int(heightA), int(heightB))
     
-        # construct a set of destination points to obtain a top
-        # down view of the image
-        # we specify the points in the smae order
+        # construct a set of destination points to obtain a top-down view of the image
+        # we specify the points in the same order
         dst = np.array([[0,0], [mxW -1, 0], [mxW -1, mxH -1], [0, mxH -1]], dtype="float32")
         # compute homography matrix
         M = cv2.getPerspectiveTransform(rect, dst)
@@ -71,7 +70,7 @@ def augment(eindex, frame, corners, image_dim, video_frame, frame_dim, image):
     M_inverse = np.linalg.inv(M)
     back_warp = cv2.warpPerspective(video_frame, M_inverse, (frame_width, frame_height))
 
-    # Load two images
+    # load two images
     img2 = back_warp
     # create an ROI representing the top left corner
     rows, cols, channels = img2.shape
